bc7455904851a198f32c66648294d1133f982d21
Hyungsuk Kang <hskang9@gmail.com>
2024-11-26 14:24:20 +0900

    add more structured test

--- packages/core/src/drizzle/runtime.test.ts
+++ packages/core/src/drizzle/runtime.test.ts
@@ -42,6 +42,112 @@ test("runtime insert", async (context) => {
   await cleanup();
 });
 
+test(
+  "runtime insert Token",
+  async (context) => {
+    const schema = createSchema((p) => ({
+      Token: p.createTable({
+        /// address of the token contract
+        id: p.string(),
+        /// token name
+        name: p.string(),
+        /// token symbol
+        symbol: p.string(),
+        /// total supply
+        totalSupply: p.float(),
+        /// Logo URL
+        logoURI: p.string(),
+        /// token decimals
+        decimals: p.int(),
+        /// price in DEX in USDT
+        price: p.float(),
+        /// price in USD based on coin portals such as CMC or CG
+        cpPrice: p.float(),
+        /// Coingecko id
+        cgId: p.string(),
+        /// Coinmarketcap id
+        cmcId: p.string(),
+        /// All Time High
+        ath: p.float(),
+        /// All Time Low
+        atl: p.float(),
+        /// liked by users
+        likes: p.int(),
+        /// creator
+        creator: p.string(),
+        /// total Hour Buckets
+        totalHourBuckets: p.int(),
+        /// total Day Buckets
+        totalDayBuckets: p.int(),
+        /// total Week Buckets
+        totalWeekBuckets: p.int(),
+        /// total Month Buckets
+        totalMonthBuckets: p.int(),
+      }),
+    }));
+
+    const { database, cleanup, indexingStore } = await setupDatabaseServices(
+      context,
+      { schema },
+    );
+
+    await indexingStore.create({
+      tableName: "Token",
+      id: "kyle",
+      data: {
+        name: "kyle",
+        symbol: "kyle",
+        totalSupply: 1.0,
+        logoURI: "kyle",
+        decimals: 1,
+        price: 1.0,
+        cpPrice: 1.0,
+        cgId: "kyle",
+        cmcId: "kyle",
+        ath: 1.0,
+        atl: 1.0,
+        likes: 1,
+        creator: "kyle",
+        totalHourBuckets: 1,
+        totalDayBuckets: 1,
+        totalWeekBuckets: 1,
+        totalMonthBuckets: 1,
+      },
+    });
+    await (indexingStore as HistoricalStore).flush({ isFullFlush: true });
+
+    const db = createDrizzleDb(database) as unknown as DrizzleDb;
+
+    const drizzleTables = createDrizzleTables(schema, database) as Context<
+      typeof schema
+    >["tables"];
+
+    await db.insert(drizzleTables.Token).values({
+      id: "kyle2",
+      name: "kyle",
+      symbol: "kyle",
+      totalSupply: 1.0,
+      logoURI: "kyle",
+      decimals: 1,
+      price: 1.0,
+      cpPrice: 1.0,
+      cgId: "kyle",
+      cmcId: "kyle",
+      ath: 1.0,
+      atl: 1.0,
+      likes: 1,
+      creator: "kyle",
+      totalHourBuckets: 1,
+      totalDayBuckets: 1,
+      totalWeekBuckets: 1,
+      totalMonthBuckets: 1,
+    });
+
+    await cleanup();
+  },
+  { timeout: 100000 },
+);
+
 test("runtime select", async (context) => {
   const schema = createSchema((p) => ({
     table: p.createTable({
